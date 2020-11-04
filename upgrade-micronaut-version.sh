5f54fcb7ff09f63a955023c1de4fe31562780b09
Iván López <lopezi@objectcomputing.com>
2020-11-04 11:04:14 +0100

    Add all the repositories

--- upgrade-micronaut-version.sh
+++ upgrade-micronaut-version.sh
@@ -10,16 +10,8 @@ REPOS="micronaut-aws-app-graal micronaut-basic-app micronaut-cache-graal microna
        micronaut-function-aws-graal micronaut-function-graal micronaut-grpc-graal micronaut-introspected-graal \
        micronaut-management-graal micronaut-rabbitmq-graal micronaut-redis-graal micronaut-schedule-graal \
        micronaut-security-basic-auth-graal micronaut-security-cookie-graal micronaut-security-jwt-graal \
-       micronaut-security-session-graal micronaut-service-discovery-consul micronaut-service-discovery-eureka"
-
-#micronaut-data-jdbc-graal
-#micronaut-data-jpa-graal
-#micronaut-flyway-graal
-#micronaut-jooq-graal
-#micronaut-liquibase-graal
-#micronaut-servlet-graal
-#micronaut-views-graal
-#micronaut-zipkin-graal
+       micronaut-security-session-graal micronaut-service-discovery-consul micronaut-service-discovery-eureka \
+       micronaut-zipkin-graal"
 
 COMMIT_MSG="Upgrade Micronaut to ${NEW_MICRONAUT_VERSION}.BUILD-SNAPSHOT"
 
@@ -28,7 +20,7 @@ cd ${TMP_DIR}
 
 for REPO in $REPOS; do
   echo "***************************************************************************************"
-  echo "Upgrading repository: ${REPO}"
+  figlet ${REPO} -w 200
   git clone git@github.com:micronaut-graal-tests/${REPO}.git
   cd ${REPO}
   git checkout $REPO_BRANCH
@@ -37,6 +29,111 @@ for REPO in $REPOS; do
   cd ..
   echo "***************************************************************************************"
 done
+
+REPO="micronaut-data-jdbc-graal"
+echo "***************************************************************************************"
+figlet ${REPO} -w 200
+git clone git@github.com:micronaut-graal-tests/${REPO}.git
+cd ${REPO}
+DATABASES="h2 mariadb mysql oracle postgres sqlserver"
+for DATABASE in $DATABASES; do
+  git checkout ${REPO_BRANCH}_${DATABASE}
+  sed -i "s/${OLD_MICRONAUT_VERSION}/${NEW_MICRONAUT_VERSION}/g" gradle.properties
+  git add . && git commit -m "${COMMIT_MSG}" && git push
+  echo "***************************************************************************************"
+done
+cd ..
+
+
+REPO="micronaut-data-jpa-graal"
+echo "***************************************************************************************"
+figlet ${REPO} -w 200
+git clone git@github.com:micronaut-graal-tests/${REPO}.git
+cd ${REPO}
+DATABASES="h2 mariadb mysql oracle postgres sqlserver"
+for DATABASE in $DATABASES; do
+  git checkout ${REPO_BRANCH}_${DATABASE}
+  sed -i "s/${OLD_MICRONAUT_VERSION}/${NEW_MICRONAUT_VERSION}/g" gradle.properties
+  git add . && git commit -m "${COMMIT_MSG}" && git push
+  echo "***************************************************************************************"
+done
+cd ..
+
+
+REPO="micronaut-flyway-graal"
+echo "***************************************************************************************"
+figlet ${REPO} -w 200
+git clone git@github.com:micronaut-graal-tests/${REPO}.git
+cd ${REPO}
+DATABASES="h2 mariadb postgres"
+for DATABASE in $DATABASES; do
+  git checkout ${REPO_BRANCH}_${DATABASE}
+  sed -i "s/${OLD_MICRONAUT_VERSION}/${NEW_MICRONAUT_VERSION}/g" gradle.properties
+  git add . && git commit -m "${COMMIT_MSG}" && git push
+  echo "***************************************************************************************"
+done
+cd ..
+
+
+REPO="micronaut-jooq-graal"
+echo "***************************************************************************************"
+figlet ${REPO} -w 200
+git clone git@github.com:micronaut-graal-tests/${REPO}.git
+cd ${REPO}
+DATABASES="h2 postgres"
+for DATABASE in $DATABASES; do
+  git checkout ${REPO_BRANCH}_${DATABASE}
+  sed -i "s/${OLD_MICRONAUT_VERSION}/${NEW_MICRONAUT_VERSION}/g" gradle.properties
+  git add . && git commit -m "${COMMIT_MSG}" && git push
+  echo "***************************************************************************************"
+done
+cd ..
+
+
+#REPO="micronaut-liquibase-graal"
+#echo "***************************************************************************************"
+#figlet ${REPO} -w 200
+#git clone git@github.com:micronaut-graal-tests/${REPO}.git
+#cd ${REPO}
+#DATABASES="h2 mariadb postgres"
+#for DATABASE in $DATABASES; do
+#  git checkout ${REPO_BRANCH}_${DATABASE}
+#  sed -i "s/${OLD_MICRONAUT_VERSION}/${NEW_MICRONAUT_VERSION}/g" gradle.properties
+#  git add . && git commit -m "${COMMIT_MSG}" && git push
+#  echo "***************************************************************************************"
+#done
+#cd ..
+
+
+REPO="micronaut-servlet-graal"
+echo "***************************************************************************************"
+figlet ${REPO} -w 200
+git clone git@github.com:micronaut-graal-tests/${REPO}.git
+cd ${REPO}
+SERVLETS="tomcat jetty"
+for SERVER in $SERVLETS; do
+  git checkout ${REPO_BRANCH}_${SERVER}
+  sed -i "s/${OLD_MICRONAUT_VERSION}/${NEW_MICRONAUT_VERSION}/g" gradle.properties
+  git add . && git commit -m "${COMMIT_MSG}" && git push
+  echo "***************************************************************************************"
+done
+cd ..
+
+
+REPO="micronaut-views-graal"
+echo "***************************************************************************************"
+figlet ${REPO} -w 200
+git clone git@github.com:micronaut-graal-tests/${REPO}.git
+cd ${REPO}
+VIEWS="freemarker handlebars thymeleaf velocity"
+for VIEW in $VIEWS; do
+  git checkout ${REPO_BRANCH}_${VIEW}
+  sed -i "s/${OLD_MICRONAUT_VERSION}/${NEW_MICRONAUT_VERSION}/g" gradle.properties
+  git add . && git commit -m "${COMMIT_MSG}" && git push
+  echo "***************************************************************************************"
+done
+cd ..
+
 
 # Cleanup
 rm -rf $TMP_DIR
