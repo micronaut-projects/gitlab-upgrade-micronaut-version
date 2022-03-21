c2ee732741cee42c0e5f6dd585b6d0e11f161bdf
Sergio del Amo <sergio.delamo@softamo.com>
2022-03-21 05:50:50 +0100

    add micronaut-kotlin-graal repo

--- upgrade-gradle-version.sh
+++ upgrade-gradle-version.sh
@@ -64,6 +64,7 @@ upgradeSingleBranchRepo "micronaut-aws-app-graal \
       micronaut-grpc-graal \
       micronaut-introspected-graal \
       micronaut-kafka-graal \
+      micronaut-kotlin-graal \
       micronaut-maven-graal \
       micronaut-management-graal \
       micronaut-rabbitmq-graal \
